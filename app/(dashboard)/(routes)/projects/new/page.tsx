54af4a863ebd15cc465718463dc808183de75304
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-01-28 00:56:02 +0300

    update

--- app/(dashboard)/(routes)/projects/new/page.tsx
+++ app/(dashboard)/(routes)/projects/new/page.tsx
@@ -3,15 +3,25 @@ import { Separator } from "@/components/ui/separator";
 import { FormProject } from "./components/form-project";
 import axios from "axios";
 export default async function NewProject() {
-  const restec = await axios.get("http://127.0.0.1:8000/technology/index");
-  const restoo = await axios.get("http://127.0.0.1:8000/technology/index");
-  const resmem = await axios.get("http://127.0.0.1:8000/member/index");
-  const reswork = await axios.get("http://127.0.0.1:8000/work_types/index");
-  const resplat = await axios.get("http://127.0.0.1:8000/platform/index");
+  const restec = await axios.get(
+    "https://backend.microtechdev.com/micro_tech/technology/index"
+  );
+  const restoo = await axios.get(
+    "https://backend.microtechdev.com/micro_tech/tool/index"
+  );
+  const resmem = await axios.get(
+    "https://backend.microtechdev.com/micro_tech/member/index"
+  );
+  const reswork = await axios.get(
+    "https://backend.microtechdev.com/micro_tech/work_types/index"
+  );
+  const resplat = await axios.get(
+    "https://backend.microtechdev.com/micro_tech/platform/index"
+  );
   const technologies = await restec.data.data;
   const tools = await restoo.data.data;
   const members = await resmem.data.data;
-  const work_types = await resplat.data.data;
+  const work_types = await reswork.data.data;
   const platforms = await resplat.data.data;
 
   return (
