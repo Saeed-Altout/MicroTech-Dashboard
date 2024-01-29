3d0c088c806abbe7852fe8eca908fac155d2c23a
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-01-30 01:15:23 +0300

    create function getdata

--- app/(dashboard)/(routes)/technologies/page.tsx
+++ app/(dashboard)/(routes)/technologies/page.tsx
@@ -1,17 +1,15 @@
-import axios from "axios";
-import { TechnologyColumn } from "@/config/config";
-
 import { columns } from "./components/columns";
 import { Client } from "./components/client";
 
 import { Separator } from "@/components/ui/separator";
 import { DataTable } from "@/components/ui/data-table";
 
+import { getData } from "@/helpers/get-data";
+import { TechnologyColumn } from "@/config/config";
+
 export default async function Technologies() {
-  const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/technology/index`
-  );
-  const data: TechnologyColumn[] = await res.data.data;
+  const res = await getData("technology");
+  const data: TechnologyColumn[] = await res.data;
 
   return (
     <>
