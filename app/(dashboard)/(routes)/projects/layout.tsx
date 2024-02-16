0a9cb9d72dcd2da14b94bdd0145ec8163fd75217
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-02-16 03:12:40 +0300

    fixed SSR

--- app/(dashboard)/(routes)/projects/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { ProjectContext } from "@/providers/project-provider";
-
-export default function ProjectLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <ProjectContext>{children}</ProjectContext>;
-}
