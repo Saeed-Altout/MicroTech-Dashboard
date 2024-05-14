77b1f60d15b53432cad6bdd04f192ba3da6656fb
Saeed-Altout <saeedaltoutpro@gmail.com>
2024-05-14 23:25:47 +0300

    remove all redirect project to find way better from this

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,17 +1,8 @@
-import { cookies } from "next/headers";
-import { redirect } from "next/navigation";
-
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const isLoggedIn = cookies().has("access-token");
-
-  if (isLoggedIn) {
-    redirect("/dashboard");
-  }
-
   return (
     <div className="h-full flex justify-center items-center">{children}</div>
   );
